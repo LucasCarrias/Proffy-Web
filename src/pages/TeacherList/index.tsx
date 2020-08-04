14c8ef89f76f3d7b290b230a0fb153d781223ee3
Lucas Carrias <lucascarrias@outlook.com>
2020-08-03 21:07:25 -0300

    Criar e estilizar item no TeacherPage 'mobile'

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import "./styles.css";
 
@@ -27,6 +28,39 @@ function TeacherList(){
                     </div>
                 </form>
             </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars3.githubusercontent.com/u/24925816?s=460&u=df080e98a2d7cfcc6d3013bcedcba40b74e036be&v=4" alt="Lucas Carrias"/>
+                        <div>
+                            <strong>Lucas Carrias</strong>
+                            <span>Python</span>
+                        </div>
+                    </header>
+
+                    <p>
+                        Aluno de Análise e Desenvolvimento de Sistemas.abs
+                        <br/><br/>
+                        Apaixonado por invertar coisas aleatórias e tentar automatizar tudo (que é chato) na vida. Meu foco é em web scrapping, hacking e desenvolvimento web;
+
+                    </p>
+
+                    <footer>
+                        <p>
+                            Preço/hora
+                            <strong>R$ 40.00</strong>
+                        </p>
+                        
+                        <button type="button">
+                        <img src={whatsappIcon} alt="WhatsApp"/>
+                        Entrar em contato
+                    </button>
+                    </footer>
+                    
+
+                </article>
+            </main>
         </div>  
     )
 }
