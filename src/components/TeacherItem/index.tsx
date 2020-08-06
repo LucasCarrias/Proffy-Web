b2fbfe6cc7def954877beb628a581f78e58f1697
Lucas Carrias <lucascarrias@outlook.com>
2020-08-05 22:49:49 -0300

    Listar professores na TeacherList

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,35 +4,44 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
 
+export interface Teacher {    
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;        
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+interface TeacherItemProps {
+    teacher: Teacher;
+}
 
-function TeacherItem(){
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return (
         <article className="teacher-item">
                     <header>
-                        <img src="https://avatars3.githubusercontent.com/u/24925816?s=460&u=df080e98a2d7cfcc6d3013bcedcba40b74e036be&v=4" alt="Lucas Carrias"/>
+                        <img src={teacher.avatar} alt={teacher.name}/>
                         <div>
-                            <strong>Lucas Carrias</strong>
-                            <span>Web Scraping</span>
+                            <strong>{teacher.name}</strong>
+                            <span>{teacher.subject}</span>
                         </div>
                     </header>
 
                     <p>
-                        Aluno de Análise e Desenvolvimento de Sistemas.
-                        <br/><br/>
-                        Apaixonado por invertar coisas aleatórias e tentar automatizar tudo (que é chato) na vida. Meu foco é em web scrapping, hacking e desenvolvimento web;
-
+                        {teacher.bio}
                     </p>
 
                     <footer>
                         <p>
                             Preço/hora
-                            <strong>R$ 40.00</strong>
+                            <strong>R$ {teacher.cost}</strong>
                         </p>
 
                         <button type="button">
-                        <img src={whatsappIcon} alt="WhatsApp"/>
-                        Entrar em contato
-                    </button>
+                            <img src={whatsappIcon} alt="WhatsApp"/>
+                            Entrar em contato
+                        </button>
                     </footer>                    
 
                 </article>
